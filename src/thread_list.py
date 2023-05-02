53fc478c46b41a54bf469daac76c4fbf0ac8d0e3
Adam Jones <admajonse@gmail.com>
2023-05-02 12:30:36 -0400

    Add type hints to thread list

--- src/thread_list.py
+++ src/thread_list.py
@@ -13,31 +13,31 @@ class ThreadList:
     def __init__(self):
         self.threads : List[GameThread] = []
 
-    def set(self, threads : List[GameThread]):
+    def set(self, threads : List[GameThread]) -> None:
         """
         TODO
         """
         self.threads = threads
 
-    def append(self, thread : GameThread):
+    def append(self, thread : GameThread) -> None:
         """
         TODO
         """
         self.threads.append(thread)
 
-    def clear(self):
+    def clear(self) -> None:
         """
         TODO
         """
         self.threads = []
 
-    def get(self):
+    def get(self) -> List[GameThread]:
         """
         TODO
         """
         return self.threads
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         """
         TODO
         """
