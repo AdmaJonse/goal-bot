5bf679c4aac85f47910005722025fb21b87e1f08
Adam Jones <admajonse@gmail.com>
2023-11-10 20:26:11 -0500

    Add error log with reason when we skip posting a highlight

--- src/command/post_highlight.py
+++ src/command/post_highlight.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 from src.command.command import Command, Priority
 from src.data.highlight import Highlight
+from src.logger import log
 from src.output import output
 
 class PostHighlight(Command):
@@ -24,7 +25,17 @@ class PostHighlight(Command):
         """
         text   : Optional[str] = self.highlight.get_post()
         footer : Optional[str] = self.highlight.get_footer()
-        if (text is not None and
-            footer is not None and
-            not output.has_posted_today(footer.strip())):
-            output.post_with_media(text, self.highlight.video)
+
+        if text is None:
+            log.error("Could not post highlight - no post text.")
+            return
+
+        if footer is None:
+            log.error("Could not post highlight - no footer text")
+            return
+
+        if output.has_posted_today(footer.strip()):
+            log.error("Could not post highlight - duplicate post")
+            return
+
+        output.post_with_media(text, self.highlight.video)
