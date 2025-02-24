06067987c66504a11795541ff568669e92d913b4
Adam Jones <admajonse@gmail.com>
2025-02-24 10:42:03 -0500

    resolve linter errors

--- src/data/game_type.py
+++ src/data/game_type.py
@@ -8,6 +8,23 @@ from typing import Dict
 class GameType(Enum):
     """
     Enumerated type capturing the possible states of a game.
+
+    https://github.com/Zmalski/NHL-API-Reference/issues/23#issuecomment-2492925102
+
+    1 - pre-season (1,998 games)
+    2 - regular season (63,930)
+    3 - playoffs (5,017)
+    4 - all-star (84)
+    6 - World Cup group stage (36)
+    7 - World Cup knockout stage (18)
+    8 - World Cup pre-tournament (12)
+    9 - Olympics (168)
+    10 - Young Stars (2)
+    12 - Canadian All-Stars vs. American All-Stars (1) and Team King vs. Team Kloss (1) -> 2 total
+    13 - games lost to the 2004 labor dispute (1,230)
+    14 - Canada Cup (93)
+    18 - appears to be exhibition games played overseas (36)
+    19 - Four Nations Face-off (6)
     """
     EXHIBITION = 1
     REGULAR_SEASON = 2
@@ -38,7 +55,7 @@ class GameType(Enum):
         Return a boolean indicating whether or not this is a Four Nations
         tournament game.
         """
-        return self == GameType.FOUR_NATIONS or self == GameType.FOUR_NATIONS_FINAL
+        return self in (GameType.FOUR_NATIONS, GameType.FOUR_NATIONS_FINAL)
 
     def is_four_nations_final(self) -> bool:
         """
@@ -49,9 +66,9 @@ class GameType(Enum):
 
 
 game_type_lookup : Dict[int, GameType] = {
-    1: GameType.EXHIBITION,
-    2: GameType.REGULAR_SEASON,
-    3: GameType.PLAYOFF,
+    1:  GameType.EXHIBITION,
+    2:  GameType.REGULAR_SEASON,
+    3:  GameType.PLAYOFF,
     19: GameType.FOUR_NATIONS,
     20: GameType.FOUR_NATIONS_FINAL
 }
