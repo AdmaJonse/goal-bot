41b0dcb3e3e1b3d8671f59fd17fba38c321a7330
Adam Jones <admajonse@gmail.com>
2026-04-18 10:47:54 -0400

    Re-enable Twitter posting.

--- src/output/output.py
+++ src/output/output.py
@@ -7,6 +7,7 @@ from typing import Dict, List, Optional
 from src.output.outputter import Outputter
 from src.output.printer import Printer
 from src.output.bluesky import BlueSky
+from src.output.tweeter import Tweeter
 
 
 class Output:
@@ -23,6 +24,7 @@ class Output:
             self._outputters.append(Printer())
         else:
             self._outputters.append(BlueSky())
+            self._outputters.append(Tweeter())
 
     @property
     def dry_run(self) -> bool:
